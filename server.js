ed2c4c5bd1cbfc2165054eb10854e208965ec57e
Thanh Van <thanhvannguyen.foreducation@gmail.com>
2017-11-16 12:27:50 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const hbs = require('hbs');
+const express= require('express');
+const fs=require('fs');
+
+let app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('toUpperCase',(text)=>{
+    return text.toUpperCase();
+});
+app.set('view engine','hbs');
+
+//create a log for server
+app.use((req,res,next)=>{
+   let timeStamp=new Date().toString();
+   let log=`${timeStamp}: ${req.method} ${req.url}`;
+   console.log(log);
+   fs.writeFileSync("server log",log+'\n');
+   next();
+})
+//maintanace page
+app.use((req,res,next)=>{
+    res.render('maintanance.hbs');
+});
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'This is a welcome message'
+    })
+});
+
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        currentYear: new Date().getFullYear()
+    });
+
+})
+
+
+app.listen(3000);
